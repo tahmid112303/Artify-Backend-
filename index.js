5d37ab7eb4e514c528ca8b996a08867f09106180
Tahmid Galib <galibtahmid@gmail.com>
2026-04-30 20:10:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.PORT || 3000;
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fuph8ia.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+const myDB = client.db("artify120");
+const myColl = myDB.collection("artInfo");
+
+async function run() {
+  try {
+  
+    app.post('/arts', async(req,res)=>{
+        const newArt = req.body
+        const result = await myColl.insertOne(newArt)
+        res.send(result)
+    })
+
+    app.get('/arts', async(req,res)=>{
+      const cursor = myColl.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    app.get('/arts/:id', async(req,res)=>{
+      const id = req.params.id
+      const query = {_id : new ObjectId(id)}
+      const result = await myColl.findOne(query)
+      res.send(result)
+    })
+
+    app.get('/artsHome', async(req,res)=>{
+      const cursor = myColl.find().sort({createdAt: -1}).limit(6)
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+      //await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
