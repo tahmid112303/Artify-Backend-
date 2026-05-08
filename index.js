a02418e8dd218decd961775fbee72bdbeba888b3
Tahmid Galib <galibtahmid@gmail.com>
2026-05-08 10:01:48 +0600

    ready to deploy

--- index.js
+++ index.js
@@ -171,7 +171,7 @@ async function run() {
     })
 
     await client.connect();
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
       //await client.close();
